ff8b4736ab52e684bd5de37b3c781a4766d3e4c1
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-07-20 18:27:26 +0200

    Add widget/gui/settings modules from biolab/orange3.

---
+++ setup.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+import sys
+
+try:
+    import setuptools
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+NAME = "Orange-Widgets-Core"
+VERSION = "0.0.0"
+DESCRIPTION = "Core orange widget utilities"
+LONG_DESCRIPTION = open("README.txt", "rt").read()
+
+URL = "http://orange.biolab.si/"
+AUTHOR = "Bioinformatics Laboratory, FRI UL"
+AUTHOR_EMAIL = 'contact@orange.biolab.si'
+
+LICENSE = "GPLv3"
+DOWNLOAD_URL = 'https://github.org/ales-erjavec/orange-widget-core'
+PACKAGES = find_packages()
+
+PACKAGE_DATA = {
+    "orangewidget": ["icons/*.svg", "icons/*png"],
+}
+
+INSTALL_REQUIRES = ("orange-canvas-core>=0.0,<0.1",)
+
+
+CLASSIFIERS = (
+    "Development Status :: 1 - Planning",
+    "Environment :: X11 Applications :: Qt",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Topic :: Scientific/Engineering :: Visualization",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+)
+
+if __name__ == "__main__":
+    setup(name=NAME,
+          version=VERSION,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          url=URL,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          license=LICENSE,
+          packages=PACKAGES,
+          package_data=PACKAGE_DATA,
+          install_requires=INSTALL_REQUIRES,
+         )
+
