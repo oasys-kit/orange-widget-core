1eaaa0a04b31fbf22c308c6acece59b09e5245e2
lucarebuffi <lrebuffi@anl.gov>
2020-01-06 11:14:21 -0600

    PyQt 5.14 bug fixed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "Oasys-Widget-Core"
-VERSION = "0.0.6"
+VERSION = "1.0.0"
 DESCRIPTION = "Core orange widget utilities"
 LONG_DESCRIPTION = open("README.txt", "rt").read()
 
