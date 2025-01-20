622093337aa739ef3b50959282271ede7980da9e
lrebuffi <lrebuffi@anl.gov>
2025-01-20 08:53:28 -0600

    replaced deprecated pkg_resources with importlib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "Oasys-Widget-Core"
-VERSION = "1.0.3"
+VERSION = "1.0.4"
 DESCRIPTION = "Core orange widget utilities"
 LONG_DESCRIPTION = open("README.txt", "rt").read()
 
